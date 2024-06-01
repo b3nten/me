c56b92ab2c467f7258b4c47a680dc91091caa497
benten <ben@benten.ca>
2024-06-01 01:03:02 -0600

    remove idea crud

--- engine/mod.ts
+++ engine/mod.ts
@@ -1,5 +1,4 @@
-import * as three from "three";
-import {EffectComposer, EffectPass, FXAAEffect, RenderPass, ToneMappingEffect, ToneMappingMode} from "postprocessing";
+import { Scene, Group, Object3D } from "three";
 
 /***********************************************************
     Generic Types
@@ -10,11 +9,6 @@ type ConstructorOf<T, Args extends any[] = any[]> = (new (
 ) => T)
 
 type Component = any
-
-const ActiveCameraTag = Symbol();
-const RendererTag = Symbol();
-const ClockTag = Symbol();
-const PostProcessingTag = Symbol();
 
 interface ILifecycle {
 	onCreate?(): void | Promise<void>;
@@ -28,169 +22,193 @@ interface ILifecycle {
     GameObject Container
  ************************************************************/
 
-interface IWorld {
+interface IGameObjectContainer {
 	addComponentToGameObject<T extends Component | Promise<Component>>(gameObject: GameObject, component: T): T;
 
 	removeComponentFromGameObject<T extends Component>(gameObject: GameObject, component: T): T;
 
-	enableGameObject<T extends GameObject>(gameObject: T): T;
-
-	disableGameObject<T extends GameObject>(gameObject: T): T;
-
-	addChildToGameObject<T extends GameObject, U extends GameObject>(gameObject: T, child: U): U;
-
-	removeChildFromGameObject<T extends GameObject, U extends GameObject>(gameObject: T, child: U): U;
-
 	addBehaviorToGameObject<T extends Behavior, Args extends any[]>
 	(gameObject: GameObject, behavior: ConstructorOf<T, [GameObject, ...Args]>, ...args: Args): T;
 
+	removeBehaviorFromGameObject<T extends Behavior>(behavior: T): T;
+
+	destroyGameObject<T extends GameObject>(gameObject: T): T;
+
 	destroyBehavior<T extends Behavior>(behavior: T): T;
-
-	enableBehavior<T extends Behavior>(behavior: T): T;
-
-	disableBehavior<T extends Behavior>(behavior: T): T;
 }
 
-class World implements IWorld {
+class ThreeGameObjectContainer implements IGameObjectContainer {
 
-	constructor() {
-		this.#camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-		this.#renderer = new three.WebGLRenderer({
-			antialias: true,
-			alpha: true,
-		});
-		this.#renderer.shadowMap.enabled = true;
-		this.#renderer.domElement.style.width = "100%";
-		this.#renderer.domElement.style.height = "100%";
-		this.#postprocessing = new EffectComposer(this.#renderer, {
-			frameBufferType: three.HalfFloatType,
-		});
-		this.#passes = [
-			new RenderPass(this.#root, this.#camera),
-			new EffectPass(this.#camera, new FXAAEffect()),
-			new EffectPass(
-				this.#camera,
-				new ToneMappingEffect({
-					mode: ToneMappingMode.ACES_FILMIC,
-				})
-			)
-		]
-	}
+	#root = new Scene
 
-	#root = new three.Scene
-	#camera: three.Camera;
-	#clock = new three.Clock;
-	#renderer: three.WebGLRenderer;
+	private gameObjects: Map<GameObject, Group> = new Map;
 
-	#postprocessing: EffectComposer;
-	#passes: Array<EffectPass | RenderPass> = []
-	public setRenderPasses(value: Array<EffectPass | RenderPass>) {
-		this.#passes = value;
-		this.#postprocessing.removeAllPasses();
-		for (const pass of this.#passes) {
-			this.#postprocessing.addPass(pass);
+	#state: "idle" | "running" | "destroyed" = "idle"
+	get state() { return this.#state }
+
+	public run(): void {
+		if(this.#state !== "idle") return;
+		this.#state = "running";
+		for(const [gameObject] of this.gameObjects) {
+			this.#startGameObject(gameObject);
 		}
 	}
 
-	private gameObjects: Set<GameObject> = new Set;
-
-	public start(): void {
-		// initialize all game objects
-	}
-
 	public tick(delta: number, elapsed: number): void {
-		// update all game objects
+		if(this.#state !== "running") return;
+		for(const [gameObject] of this.gameObjects) {
+			this.#updateGameObject(gameObject, delta, elapsed);
+		}
 	}
 
 	public destroy(): void {
-		// destroy all game objects
+		if(this.#state !== "running") return;
+		this.#state = "destroyed";
+		for(const [gameObject] of this.gameObjects) {
+			this.destroyGameObject(gameObject);
+		}
 	}
 
 	addGameObject<T extends GameObject, Args extends any[]>(gameObject: ConstructorOf<T, Args>, ...args: Args): T {
 		const instance = GameObject.Create(this, gameObject, ...args);
-
+		const group = new Group();
+		this.#root.add(group);
+		this.gameObjects.set(instance, group)
+		if(this.#state === "running") {
+			this.#startGameObject(instance);
+		}
 		return instance;
 	}
 
 	destroyGameObject<T extends GameObject>(gameObject: T): T {
-
+		this.#destroyGameObject(gameObject);
+		return gameObject;
 	}
 
-	enableGameObject<T extends GameObject>(gameObject: T): T {
-		console.log("LOL")
-	}
-
-	disableGameObject<T extends GameObject>(gameObject: T): T {
-
-	}
 
 	addComponentToGameObject<T extends Component>(gameObject: GameObject, component: T): T {
-
+		gameObject.components.push(component);
+		const group = this.gameObjects.get(gameObject);
+		if(group && component instanceof Object3D) {
+			group.add(component);
+		}
+		return component;
 	}
 
 	removeComponentFromGameObject<T extends Component>(gameObject: GameObject, component: T): T {
-
+		const index = gameObject.components.indexOf(component);
+		if(index !== -1) {
+			gameObject.components.splice(index, 1);
+		}
+		const group = this.gameObjects.get(gameObject);
+		if(group && component instanceof Object3D) {
+			group.remove(component);
+		}
+		return component;
 	}
 
 	addChildToGameObject<T extends GameObject, U extends GameObject>(gameObject: T, child: U): U {
-		throw Error("Method not implemented.");
+
 	}
 
 	removeChildFromGameObject<T extends GameObject, U extends GameObject>(gameObject: T, child: U): U {
-		throw new Error("Method not implemented.");
+
 	}
 
 	addBehaviorToGameObject<T extends Behavior, Args extends any[]>(gameObject: GameObject, behavior: ConstructorOf<T, [GameObject, ...Args]>, ...args: Args): T {
-
+		const instance = Behavior.Create(gameObject, behavior, ...args);
+		gameObject.behaviors.push(instance);
+		return instance;
 	}
 
 	destroyBehavior<T extends Behavior>(behavior: T): T {
-
+		this.#destroyBehavior(behavior);
+		return behavior;
 	}
 
-	enableBehavior<T extends Behavior>(behavior: T): T {
-
+	#startGameObject(gameObject: GameObject): void {
+		if(this.#state !== "running") return;
+		gameObject.onCreate?.();
+		gameObject.created = true;
+		for(const behavior of gameObject.behaviors) {
+			this.#startBehavior(behavior);
+		}
 	}
 
-	disableBehavior<T extends Behavior>(behavior: T): T {
-
+	#updateGameObject(gameObject: GameObject, delta: number, elapsed: number): void {
+		if(this.#state !== "running") return;
+		if(gameObject.enabled){
+			if(!gameObject.created) this.#startGameObject(gameObject);
+			gameObject.onUpdate?.(delta, elapsed);
+			gameObject.behaviors.forEach(behavior => {
+				this.#updateBehavior(behavior, delta, elapsed);
+			});
+		}
 	}
 
-	onCreate?(): void;
-	onUpdate?(delta: number, elapsed: number): void;
-	onDestroy?(): void;
+	#destroyGameObject(gameObject: GameObject): void {
+		if(gameObject.created && !gameObject.destroyed) {
+			gameObject.onDestroy?.();
+			gameObject.created = false;
+			gameObject.destroyed = true;
+		}
+		for(const behavior of gameObject.behaviors) {
+			this.#destroyBehavior(behavior);
+		}
+	}
+
+	#startBehavior(behavior: Behavior): void {
+		behavior.onCreate?.();
+		behavior.created = true;
+	}
+
+	#updateBehavior(behavior: Behavior, delta: number, elapsed: number): void {
+		if(behavior.enabled) {
+			if(!behavior.created) this.#startBehavior(behavior);
+			behavior.onUpdate?.(delta, elapsed);
+		}
+	}
+
+	#destroyBehavior(behavior: Behavior): void {
+		if(behavior.created && !behavior.destroyed) {
+			behavior.onDestroy?.();
+			behavior.created = false;
+			behavior.destroyed = true;
+		}
+	}
 }
 
 /***************************************************************************************
  GAME OBJECT
  ***************************************************************************************/
 
-let currentScene: IWorld | null = null;
-const setCurrentScene = (scene: IWorld | null) => currentScene = scene;
-const getCurrentScene = () => {
-	if(!currentScene) throw new Error("GameObject created without a provided IScene instance. If you encounter this through normal usage it's a bug.");
-	return currentScene;
+let currentGameObjectContainer: IGameObjectContainer | null = null;
+const setCurrentGameObjectContainer = (scene: IGameObjectContainer | null) => currentGameObjectContainer = scene;
+const getCurrentGameObjectContainer = () => {
+	if(!currentGameObjectContainer) throw new Error("GameObject created without a provided IScene instance. If you encounter this through normal usage it's a bug.");
+	return currentGameObjectContainer;
 }
 
 class GameObject implements ILifecycle {
 
-	static Create<T extends GameObject, Args extends any[]>(scene: IWorld, gameObject: ConstructorOf<T, Args>, ...args: Args): T {
-		setCurrentScene(scene);
+	static Create<T extends GameObject, Args extends any[]>(scene: IGameObjectContainer, gameObject: ConstructorOf<T, Args>, ...args: Args): T {
+		setCurrentGameObjectContainer(scene);
 		const instance = new gameObject(...args);
-		setCurrentScene(null);
+		setCurrentGameObjectContainer(null);
 		return instance;
 	}
 
-	scene: IWorld = getCurrentScene();
+	container: IGameObjectContainer = getCurrentGameObjectContainer();
 
 	/***************************************************************************************
 	 PROPERTIES
 	 ***************************************************************************************/
-	group: three.Group = new three.Group();
 	components: Component[] = [];
 	behaviors: Behavior[] = [];
 	children: GameObject[] = [];
 
+	created: boolean = false;
+	destroyed: boolean = false;
 	enabled: boolean = true;
 
 	tag: symbol | null = null;
@@ -201,58 +219,27 @@ class GameObject implements ILifecycle {
 	 **************************************************************************************/
 
 	onCreate?(): void;
-
-	/*
-	 * The update method is called every frame after the GameObject has been started.
-	 * The delta parameter is the time in seconds since the last frame.
-	 * The elapsed parameter is the total time in seconds since the world started.
-	 */
 	onUpdate?(delta: number, elapsed: number): void;
-
-	/*
-	 * The destroy method is called once when the GameObject is destroyed.
-	 * This method is called after the GameObject is removed from the world.
-	 */
 	onDestroy?(): void;
-
-	onEnable?(): void;
-
-	onDisable?(): void;
 
 	/***************************************************************************************
 	 METHODS
 	 ***************************************************************************************/
 
-	public enable(): void {
-		this.scene.enableGameObject(this);
-	}
-
-	public disable(): void {
-		this.scene.disableGameObject(this);
-	}
-
 	public addComponent<T extends Component>(component: T): T {
-		return this.scene.addComponentToGameObject(this, component);
+		return this.container.addComponentToGameObject(this, component);
 	}
 
 	public removeComponent<T extends Component>(component: T): T {
-		return this.scene.removeComponentFromGameObject(this, component);
-	}
-
-	public addChild<T extends GameObject>(child: T): T {
-		return this.scene.addChildToGameObject(this, child);
-	}
-
-	public removeChild<T extends GameObject>(child: T): T {
-		return this.scene.removeChildFromGameObject(this, child);
+		return this.container.removeComponentFromGameObject(this, component);
 	}
 
 	public addBehavior<T extends Behavior, Args extends any[]>(behavior: ConstructorOf<T, [GameObject, ...Args]>, ...args: Args): T {
-		return this.scene.addBehaviorToGameObject(this, behavior, ...args);
+		return this.container.addBehaviorToGameObject(this, behavior, ...args);
 	}
 
 	public removeBehavior<T extends Behavior>(behavior: T): T {
-		return this.scene.destroyBehavior(behavior);
+		return this.container.destroyBehavior(behavior);
 	}
 
 	/***************************************************************************************
@@ -260,24 +247,7 @@ class GameObject implements ILifecycle {
 	 ***************************************************************************************/
 
 	onClick?(): void;
-
-	onMouseEnter?(): void;
-
-	onMouseOver?(): void;
-
-	onMouseLeave?(): void;
-
-	onMouseDown?(): void;
-
-	onMouseUp?(): void;
-
-	onWheel?(): void;
-
 	onKeyPress?(): void;
-
-	onKeyDown?(): void;
-
-	onKeyUp?(): void;
 }
 
 /***************************************************************************************
@@ -301,6 +271,8 @@ abstract class Behavior implements ILifecycle {
 	/***************************************************************************************
 	 PROPERTIES
 	 ***************************************************************************************/
+	created: boolean = false;
+	destroyed: boolean = false;
 	enabled: boolean = true;
 
 	tag?: symbol;
@@ -316,110 +288,18 @@ abstract class Behavior implements ILifecycle {
 
 	onDestroy?(): void;
 
-	onEnable?(): void;
-
-	onDisable?(): void;
-
-	/***************************************************************************************
-	 METHODS
-	 ***************************************************************************************/
-
-	public enable() {
-		this.gameObject.scene.enableBehavior(this);
-	}
-
-	public disable() {
-		this.gameObject.scene.disableBehavior(this);
-	}
-
 	/***************************************************************************************
 	 EVENTS
 	 ***************************************************************************************/
 
 	onClick?(): void;
-
-	onMouseEnter?(): void;
-
-	onMouseOver?(): void;
-
-	onMouseLeave?(): void;
-
-	onMouseDown?(): void;
-
-	onMouseUp?(): void;
-
-	onWheel?(): void;
-
 	onKeyPress?(): void;
-
-	onKeyDown?(): void;
-
-	onKeyUp?(): void;
-}
-
-/***************************************************************************************
- ASSET LOADER
- ***************************************************************************************/
-
-class AssetLoader {
-	#loaders = new Map<new () => three.Loader<any>, three.Loader<any>>();
-
-	#cache = new Map<string, any>();
-	#asyncCache = new Map<string, Promise<any>>();
-
-	#loaded = false;
-
-	get Ready() {
-		return Promise.all(Array.from(this.#asyncCache.values()));
-	}
-
-	get loaded() {
-		return this.#loaded;
-	}
-
-	async load<T>(
-		loader: new () => three.Loader<T>,
-		url: string
-	): Promise<T | undefined> {
-		if (typeof window === "undefined") return;
-
-		if (this.#asyncCache.has(url)) {
-			return this.#asyncCache.get(url);
-		}
-
-		const l =
-			this.#loaders.get(loader) ??
-			(() => {
-				const l = new loader();
-				this.#loaders.set(loader, l);
-				return l;
-			})();
-
-		const promise = l.loadAsync(url).then((result) => {
-			this.#cache.set(url, result);
-			return result;
-		});
-
-		this.#asyncCache.set(url, promise);
-
-		return promise;
-	}
-
-	async preload<T>(loader: new () => three.Loader<T>, url: string) {
-		this.load(loader, url);
-	}
 }
 
 export {
 	GameObject,
 	Behavior,
-	AssetLoader,
-	World,
-	ActiveCameraTag,
-	RendererTag,
-	ClockTag,
-	PostProcessingTag,
 	type Component,
-	type IWorld,
+	type IGameObjectContainer,
 	type ILifecycle
 }
