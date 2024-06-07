42841224479f68f183045270673dfb695aa658f6
benten <ben@benten.ca>
2024-06-06 23:13:04 -0600

    checkpoint

--- src/routes/bio/+page.svelte
+++ src/routes/bio/+page.svelte
@@ -3,5 +3,7 @@
 </script>
 
 <BasePageLayout>
-
+	<div class='w-full min-h-90 rounded-xl border border-primary-100/50 bg-gray-500/50 backdrop-blur-lg p-8 text-primary-100'>
+		<h1 class='text-4xl md:text-7xl font-display'>BIO</h1>
+	</div>
 </BasePageLayout>
--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,7 @@
 import {sveltekit} from '@sveltejs/kit/vite';
 import {defineConfig} from 'vite';
 import unocss from "unocss/vite"
-import {presetWebFonts, defineConfig as defineUno, presetWind} from "unocss";
-
-const uno = defineUno({
-	presets: [
-		presetWind(),
-		presetWebFonts({
-			provider: 'google',
-			fonts: {
-				sans: 'Inter',
-				display: "Play",
-				mono: ['Fira Code'],
-			},
-		})
-	],
-	theme: {
-		fontFamily: {
-			serif: ["Georgia", "serif"],
-		},
-		colors: {
-			primary: {
-				'50':  'rgba(var(--primary-50))',
-				'100': 'rgba(var(--primary-100))',
-				'200': 'rgba(var(--primary-200))',
-				'300': 'rgba(var(--primary-300))',
-				'400': 'rgba(var(--primary-400))',
-				'500': 'rgba(var(--primary-500))',
-				'600': 'rgba(var(--primary-600))',
-				'700': 'rgba(var(--primary-700))',
-				'800': 'rgba(var(--primary-800))',
-				'900': 'rgba(var(--primary-900))',
-				'950': 'rgba(var(--primary-950))',
-			}
-		}
-	}
-})
 
 export default defineConfig({
-	plugins: [sveltekit(), unocss(uno)]
+	plugins: [sveltekit(), unocss()]
 });
